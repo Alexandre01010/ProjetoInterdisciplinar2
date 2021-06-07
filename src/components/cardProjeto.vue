83bce21c03ced396e398173422422aa382b0eedd
Alexandre01010 <40190044@esmad.ipp.pt>
2021-06-07 14:41:12 +0100

    commit

--- src/components/cardProjeto.vue
+++ src/components/cardProjeto.vue
@@ -2,64 +2,35 @@
   <b-card class="mt-5" id="listCard">
     <b-col md="3">
       <p class="cardIdentification">
-        <b-icon
-          style="color: #0077b6; width: 19px; height: 19px"
-          :icon='propCard.nome_entidade==null? "file-earmark-code":"building"'
-          aria-hidden="true"
-        ></b-icon>
         Proposta {{ propCard.nome_entidade==null? "Projeto":"Estágio" }}
+        <b-badge class="proposalState ml-2" variant="success">Aprovado</b-badge>
       </p>
     </b-col>
     <b-col md="12">
       <b-row>
-        <b-col
-          v-if="propCard.id_tipo_estado == 3"
-          class="cardInformation"
-          md="8"
-        >
+        <b-col v-if="propCard.id_tipo_estado == 3" class="cardInformation" md="8">
           {{ propCard.titulo }}
-          <b-badge class="proposalState ml-2" variant="success"
-            >Aprovado</b-badge
-          >
           <b-col class="text-muted mt-2" md="12">
-            <p class="participants">{{ propCard.objetivos }}</p>
+            <p class="participants">Professor orientador: Alexandre Cunha</p>
           </b-col>
         </b-col>
-        <b-col
-          v-if="propCard.id_tipo_estado == 2"
-          class="cardInformation"
-          md="8"
-        >
+        <b-col v-if="propCard.id_tipo_estado == 2" class="cardInformation" md="8">
           {{ propCard.titulo }}
-          <b-badge class="proposalState ml-2" variant="danger"
-            >Para Revisão</b-badge
-          >
+          <b-badge class="proposalState ml-2" variant="danger">Para Revisão</b-badge>
           <b-col class="text-muted mt-2" md="12">
-            <p class="participants">{{ propCard.objetivos }}</p>
+            <p class="participants">Professor Orientador: {{ propCard.id_prof_orientador }}</p>
           </b-col>
         </b-col>
-        <b-col
-          v-if="propCard.id_tipo_estado == '1'"
-          class="cardInformation"
-          md="8"
-        >
+        <b-col v-if="propCard.id_tipo_estado == '1'" class="cardInformation" md="8">
           {{ propCard.titulo }}
-          <b-badge class="proposalState ml-2" variant="warning"
-            >Em análise</b-badge
-          >
+          <b-badge class="proposalState ml-2" variant="warning">Em análise</b-badge>
           <b-col class="text-muted mt-2" md="12">
-            <p class="participants">{{ propCard.objetivos }}</p>
+            <p class="participants">Professor Orientador: {{ propCard.id_prof_orientador }}</p>
           </b-col>
         </b-col>
         <b-col md="4">
-          <div class="d-flex justify-content-end">
-            <router-link
-              class="menuItems"
-              :to="{ name: 'proposalDetails', params: { proposta: propCard } }"
-              ><b-button id="btnDetails" variant="primary"
-                >Detalhes</b-button
-              ></router-link
-            >
+          <div class="d-flex justify-content-center">
+            <router-link class="menuItems" :to="{ name: 'proposalDetails', params: { proposta: propCard } }"><b-button id="btnDetails" variant="primary">Detalhes</b-button></router-link>
           </div>
         </b-col>
       </b-row>
@@ -99,5 +70,8 @@ export default {
   font-size: 12px;
   font-family: Rubik, sans-serif;
   font-weight: lighter;
+}
+.profOrientador{
+  font-size: 14px;
 }
 </style>
