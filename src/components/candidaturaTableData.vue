45833f9e332c2d87f40f664b3170af5118c1dfed
Alexandre01010 <40190044@esmad.ipp.pt>
2021-06-09 03:45:22 +0100

    commit

--- src/components/candidaturaTableData.vue
+++ src/components/candidaturaTableData.vue
@@ -2,17 +2,20 @@
 
     <b-tr id="tabelaCandidaturas" class="text-center tabledata">
         <b-td class="candTd">{{getProposal}}</b-td>
-        <b-td class="candTd"><input min="1" max="5" v-model="change" type="number"></b-td>
+        <b-td v-if="tableTr.id_tipo_estado == 1" class="candTd"><input class="editEscolhaOrdem" min="1" max="5" v-model="change" type="number"></b-td>
+        <b-td v-if="tableTr.id_tipo_estado == 3" class="candTd">{{tableTr.n_ordem_escolha}}</b-td>
         <b-td v-if="tableTr.id_tipo_estado == 1" class="candTd">
             <b-badge class="proposalState" variant="warning">Análise</b-badge>
         </b-td>
         <b-td v-if="tableTr.id_tipo_estado == 3" class="candTd">
             <b-badge class="proposalState" variant="success">Aprovado</b-badge>
         </b-td>
-        <b-td>
-            
+        <b-td class="candTd" v-if="tableTr.id_tipo_estado == 1"> 
             <b-button @click="updateCandidatura" class="btn_edit" variant="#0077B6" font-color="#0077B6"><b-icon-pencil-square class="btn ml-1" style="width: 23px; height: 23px"/></b-button>
             <b-button id="remove" class="btn ml-1"><b-icon-trash-fill style="width: 23px; height: 23px" /></b-button>
+        </b-td>
+        <b-td class="candTd" v-if="tableTr.id_tipo_estado == 3"> 
+            <p>Fechado</p>
         </b-td>
     </b-tr>
     
@@ -99,5 +102,14 @@ export default {
 }
 .proposalState{
     font-weight: lighter;
+}
+.editEscolhaOrdem{
+    font-weight: lighter;
+    border-top: 0 none;
+    border-left: 0 none;
+    border-right: 0 none;
+    border-bottom: 1px solid #c94514;
+    outline: 0;
+    text-align: right;
 }
 </style>
